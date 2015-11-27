3e166dc30214ff13320c56db830f718379f628c8
Stijn Overduin <stijn_overduin@hotmail.com>
2015-11-27 13:50:11 +0100

    3.10.6

--- Test/src/gitHub/helloworld.java
+++ Test/src/gitHub/helloworld.java
@@ -2,7 +2,7 @@ package gitHub;
 
 public class helloworld {
 	public static void main(String[] args){
-	System.out.println("weekend");
+	System.out.println("peop");
 	}
 	public String getName(){
 		return null;
